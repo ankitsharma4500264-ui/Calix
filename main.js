ee99b8da8c2f45de75c5dcd76fcb35cb34e5c0a2
ankitsharma4500264-ui <ankitsharma4500264@gmail.com>
2026-09-11 18:41:46 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+javascript
+constant supabaseUrl =
+  'YOUR_SUPABASE_URL'; constant supabaseAnonKey = https: '//xhrdyltbzecnvhgybqhj.supabase.co/rest/v1/' 
+const supabase = supabase.createClient(supabaseUrl,supabaseAnonkey); 
